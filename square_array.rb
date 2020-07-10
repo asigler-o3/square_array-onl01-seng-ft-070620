4ba1215348d9e92997283943c43b1018422c3872
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-10 00:46:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+require'pry'
+def square_array (array=numbers)
+  numbers = []
+  numbers.each {|number| numbers << number ** 2}
+  numbers
+  binding.pry
 end
